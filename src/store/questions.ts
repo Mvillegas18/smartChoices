9090a8edf49fdd7cf7007b9d25bee8a946500ca5
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-01-23 20:56:11 -0500

    feat: create Zustand store for managing quiz questions and fetching data

---
+++ src/store/questions.ts
@@ -0,0 +1,20 @@
+import { create } from 'zustand'
+import { type IQuestion } from '../types'
+
+interface state {
+    questions: IQuestion[]
+    currentQuestion: number
+    fetchQuestions: (limit: number) => Promise<void>
+}
+
+export const useQuestionStore = create<state>((set) => ({
+    questions: [],
+    currentQuestion: 0,
+    fetchQuestions: async (limit: number) => {
+        const res = await fetch('http://localhost:5173/data.json')
+        const json = await res.json()
+        const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
+
+        set({ questions })
+    },
+}))
