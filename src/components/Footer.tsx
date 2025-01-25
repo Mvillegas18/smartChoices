52176508a642a7cf90fb86dd570a902ede56df04
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-01-24 19:55:02 -0500

    feat: add reset functionality to Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,20 @@
+import { RestartAlt } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
 import { useQuestionData } from '../hooks/useQuestionData'
+import { useQuestionStore } from '../store/questions'
 
 export const Footer = () => {
     const { correct, incorrect, unanswered } = useQuestionData()
+    const reset = useQuestionStore((store) => store.reset)
     return (
         <footer style={{ marginTop: '16px' }}>
             <strong>
                 ✅ {correct} correctas - ❌{incorrect} incorrectas - ❓
                 {unanswered} sin responder
             </strong>
+            <IconButton onClick={() => reset()} color="warning" sx={{ ml: 2 }}>
+                <RestartAlt />
+            </IconButton>
         </footer>
     )
 }
